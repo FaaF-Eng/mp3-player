ebf0d91b713c52a321d0122d0cb3bd963bd9ac55
FaaF-Eng <fabio.teixeira18@hotmail.com>
2026-02-18 14:44:05 -0300

    first commit

---
+++ dev_notes.md
@@ -0,0 +1,18 @@
+Colocar no Git Ignore:
+
+node_modules/
+dist/
+build/
+out/
+.env
+
+
+Para clonar o repositório:
+git clone seu-repo
+cd seu-repo
+npm install
+npm run dev
+
+
+Para exportar como instalavel:
+npm run build
---
+++ main.js
@@ -0,0 +1,23 @@
+const {app, BrowserWindow} = require("electron");
+
+function createwindow(){
+    const win = new BrowserWindow({
+        width: 592,
+        height: 730,
+        resizable: false,
+        maximizable: false,
+        fullscreenable: false,
+        frame: false,
+        transparent: false,
+        webPreferences: {
+            contextIsolation: true
+        }
+    });
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(createwindow);
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") app.quit();
+});
